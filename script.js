6b5b0ec9468c6f2d4d9580d1827ead4fbaea9021
lethern <lethern@gmail.com>
2024-09-10 16:02:56 +0200

    1man fix

--- script.js
+++ script.js
@@ -745,7 +745,7 @@ function initMedals(){
 	};
 	gMedals[47] = {
 		name: "1 Man Run",
-		req: [	{ text: "Evac 1 man", func: ()=>{return gBank['1man']>=500;} }, ],
+		req: [	{ text: "Evac 1 man", func: ()=>{return gBank['1man']==111;} }, ],
 		reward: "Custom skin color",
 		order: 54,
 	};
